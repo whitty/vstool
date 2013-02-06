40924c52749b82d0e3b598bb178005edc6837683
Greg Whiteley <greg.whiteley@gmail.com>
2013-02-06 22:10:00 +1100

    Fix GPL description from library to program

--- lib/vstool/command.rb
+++ lib/vstool/command.rb
@@ -11,7 +11,7 @@
 #  GNU Lesser General Public License for more details.
 #
 #  You should have received a copy of the GNU Lesser General Public
-#  License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+#  License along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 module VsTool
   class Command
